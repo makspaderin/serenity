0b04afddb8d97201c8d898c103d490b0f3051d11
Maksim Paderin <Maksim Paderin>
2016-10-07 20:41:53 +0300

    Fixed button

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
 $('#ham').click(function(){
 	$('')
 });
+
+        skillsProgressBar();
+        function skillsProgressBar() {
+            $('.skillbar').each(function () {
+                $(this).find('.skillbar-bar').animate({
+                    width: $(this).attr('data-percent')
+                }, 2000);
+            });
+        };
