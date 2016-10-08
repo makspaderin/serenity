df1ce05355bb5f3f492596964ed31655fac71c1c
Katya <Katya>
2016-10-08 19:18:39 +0300

    Modified portfoio

--- js/custom.js
+++ js/custom.js
@@ -10,11 +10,40 @@ $('#close').click(function(){
 	});
 });
 
-        skillsProgressBar();
-        function skillsProgressBar() {
-            $('.skillbar').each(function () {
-                $(this).find('.skillbar-bar').animate({
-                    width: $(this).attr('data-percent')
-                }, 2000);
-            });
-        };
+skillsProgressBar();
+function skillsProgressBar() {
+    $('.skillbar').each(function () {
+        $(this).find('.skillbar-bar').animate({
+            width: $(this).attr('data-percent')
+        }, 2000);
+    });
+};
+
+$(function() {
+  $.scrollify({
+        section:".portfolio-box",
+        scrollbars:false,
+        before:function(i,panels) {
+      var ref = panels[i].attr("data-section-name");
+
+      $(".pagination .active").removeClass("active");
+
+      $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+    },
+    afterRender:function() {
+      var pagination = "<ul class=\"pagination\">";
+      var activeClass = "";
+      $(".panel").each(function(i) {
+        activeClass = "";
+        if(i===0) {
+          activeClass = "active";
+        }
+        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+      });
+
+      pagination += "</ul>";
+
+      $(".home").append(pagination);
+    }
+  });
+});
