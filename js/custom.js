e5662629ada00b9e86283d58ec08b944a96c5892
Maksim <Maksim@Maksim-MacBook.local>
2016-10-08 16:27:19 +0300

    Fixed about page for small screens 5

--- js/custom.js
+++ js/custom.js
@@ -18,20 +18,3 @@ $('#close').click(function(){
                 }, 2000);
             });
         };
-
-$.scrollify({
-        section : "section",
-        sectionName : "section-name",
-        interstitialSection : "",
-        easing: "easeOutExpo",
-        scrollSpeed: 1100,
-        offset : 0,
-        scrollbars: true,
-        standardScrollElements: "",
-        setHeights: true,
-        overflowScroll: true,
-        before:function() {},
-        after:function() {},
-        afterResize:function() {},
-        afterRender:function() {}
-    });
